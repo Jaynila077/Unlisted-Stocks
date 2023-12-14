0571fdeb9aa135008837d63fce807ab179e793f6
jaynila077 <jaynila077@gmail.com>
2023-12-14 19:04:16 +0530

    Initial Setup

---
+++ client/src/app/company_login/page.tsx
@@ -0,0 +1,8 @@
+export default function ComLogin() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>Company Login</h1>
+      </main>
+    )
+  }
+  
---
+++ client/src/app/user_login/page.tsx
@@ -0,0 +1,8 @@
+export default function UserLogin() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>User Login</h1>
+      </main>
+    )
+  }
+  

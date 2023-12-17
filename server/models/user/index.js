e32f012167cb36e868ab222b1d0e838785b9e061
varadpundlik <varadpundlik@gmail.com>
2023-12-17 19:30:35 +0530

    feat: user model

---
+++ server/models/user/index.js
@@ -0,0 +1,40 @@
+module.exports = (Sequelize, DataTypes) => {
+  const User = Sequelize.define("user", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    pan_number: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true,
+    },
+    aadhar_number: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true,
+    },
+    mobile_number: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  });
+
+  return User;
+};

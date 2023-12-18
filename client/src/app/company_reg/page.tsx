7fe4e95f6687975d7a49b1e217a050497e8e51d3
jaynila077 <jaynila077@gmail.com>
2023-12-18 19:53:37 +0530

    feat: User Auth

--- client/src/app/company_reg/page.tsx
+++ client/src/app/company_reg/page.tsx
@@ -1,7 +1,7 @@
-export default function ComLogin() {
+export default function ComReg() {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>Company Login</h1>
+        <h1>Company Registration</h1>
       </main>
     )
   }
---
+++ client/src/app/user_reg/page.tsx
@@ -0,0 +1,8 @@
+import FormReg from "./form"
+export default function UserReg() {
+    return (
+      <div>
+        <FormReg></FormReg>
+      </div>
+    )
+  }

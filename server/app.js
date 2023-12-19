b4b7f9d7eb1f1b424c515d23c27734a2c715cefe
varadpundlik <varadpundlik@gmail.com>
2023-12-19 10:06:48 +0530

    feat: sequelize setup

--- server/app.js
+++ server/app.js
@@ -1,5 +1,8 @@
 import express from "express";
 import routes from "./routes/index";
+import { Sequelize } from "sequelize";
+import config from "./config";
+const sequelize = new Sequelize(config);
 
 const app = express();
 
@@ -8,6 +11,15 @@ app.use(express.json());
 routes(app);
 
 try {
+  sequelize
+    .sync({ force: true }) // Use force: true only during development to drop tables on every sync
+    .then(() => {
+      console.log("Database synchronized");
+    })
+    .catch((error) => {
+      console.error("Error synchronizing database:", error);
+    });
+
   app.listen(5000, () => {
     console.log("server is running at port 5000");
   });

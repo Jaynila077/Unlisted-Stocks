84edf4321a2a9c538c6064e31589241f407864f2
jaynila077 <jaynila077@gmail.com>
2023-12-22 16:17:22 +0530

    Update

--- server/app.js
+++ server/app.js
@@ -26,4 +26,4 @@ async function createDBConnection() {
 }
 
 createDBConnection();
-app.listen(5000, () => console.log("Server is running on port 3000"));
+app.listen(5000, () => console.log("Server is running on port 5000"));
--- server/config/index.js
+++ server/config/index.js
@@ -4,7 +4,7 @@ dotenv.config({ path: '../config.env' });
 export default {
   host: process.env.host || 'localhost',
   user: process.env.user || 'postgres',
-  password: process.env.password || 'va$$$rad31',
+  password: process.env.password || '',
   database: process.env.database || 'unlisted_stocks',
   dialect: process.env.dialect || 'postgres',
   port: process.env.port || 5432,

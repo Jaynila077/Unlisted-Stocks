f80371b13e69a53bf79bcfd6c883274b3d78cf81
jaynila077 <jaynila077@gmail.com>
2024-01-07 10:59:26 +0530

    Update server config to use environment variables

--- server/config/index.js
+++ server/config/index.js
@@ -2,10 +2,10 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '../config.env' });
 
 export default {
-  host: process.env.host || 'localhost',
-  user: process.env.user || 'postgres',
-  password: process.env.password || 'va$$$rad31',
-  database: process.env.database || 'unlisted_stocks',
-  dialect: process.env.dialect || 'postgres',
-  port: process.env.port || 5432,
+  host: process.env.host ,
+  user: process.env.user ,
+  password: process.env.password, 
+  database: process.env.database, 
+  dialect: process.env.dialect , 
+  port: process.env.port,
 };
